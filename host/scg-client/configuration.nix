45deb5c399b2b8c725803df858ed43e406af1f60
apersomany <aperso@aperso.dev>
2026-01-15 17:23:22 +0900

    feat: configure cloudflare tunnel for scg-client

--- host/scg-client/configuration.nix
+++ host/scg-client/configuration.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   imports = [
@@ -17,7 +17,37 @@
   }];
   networking.defaultGateway = "115.145.150.1";
 
+  sops.defaultSopsFile = ../../secret/secrets.yaml;
+  sops.defaultSopsFormat = "yaml";
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  sops.secrets."cloudflare_tunnel/scg_client" = {
+     owner = "cloudflare";
+  };
+
   time.timeZone = "Asia/Seoul";
+
+  environment.systemPackages = with pkgs; [
+    cloudflared
+  ];
+
+  users.users.cloudflare = {
+    isSystemUser = true;
+    group = "cloudflare";
+  };
+  users.groups.cloudflare = {};
+
+  systemd.services.cloudflared = {
+    description = "Cloudflare Tunnel";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      User = "cloudflare";
+      Group = "cloudflare";
+      Restart = "always";
+      RestartSec = "5s";
+      ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.cloudflared}/bin/cloudflared tunnel run --token $(cat ${config.sops.secrets."cloudflare_tunnel/scg_client".path})'";
+    };
+  };
 
   system.stateVersion = "25.05";
 }
