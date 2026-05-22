71e0ce4459ce3752242751d96fc1e4658e1875e9
Donghyun Shin <aperso@aperso.dev>
2026-05-22 15:08:43 +0900

    feat(hypr): add KDE portal for compact file chooser dialogs
    Replaces the bulky GTK file chooser portal with KDE's compact
    xdg-desktop-portal-kde for org.freedesktop.impl.portal.FileChooser,
    which Brave/Chromium use for 'Save As' dialogs. The KDE file
    chooser is significantly more compact on 1.5x scaled displays.

--- modules/client/hypr.nix
+++ modules/client/hypr.nix
@@ -9,6 +9,18 @@
     enable = true;
     withUWSM = true;
   };
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-hyprland
+      kdePackages.xdg-desktop-portal-kde
+      xdg-desktop-portal-gtk
+    ];
+    config.hyprland = {
+      default = [ "hyprland" "gtk" ];
+      "org.freedesktop.impl.portal.FileChooser" = [ "kde" ];
+    };
+  };
   services.greetd = {
     enable = true;
     useTextGreeter = true;
