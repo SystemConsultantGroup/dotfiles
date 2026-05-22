d965d2153a99f41a35436f0eab350fef0a161569
Donghyun Shin <aperso@aperso.dev>
2026-05-22 15:11:38 +0900

    revert(hypr): remove KDE portal, go back to default portal setup

--- modules/client/hypr.nix
+++ modules/client/hypr.nix
@@ -9,18 +9,6 @@
     enable = true;
     withUWSM = true;
   };
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-hyprland
-      kdePackages.xdg-desktop-portal-kde
-      xdg-desktop-portal-gtk
-    ];
-    config.hyprland = {
-      default = [ "hyprland" "gtk" ];
-      "org.freedesktop.impl.portal.FileChooser" = [ "kde" ];
-    };
-  };
   services.greetd = {
     enable = true;
     useTextGreeter = true;
