0b0c4a8aed22af63d3de21cdd803610fd2c2f450
apersomany <aperso@aperso.dev>
2026-06-01 15:50:30 +0900

    feat: set Firefox as default browser via xdg.mimeApps

--- modules/client/home.nix
+++ modules/client/home.nix
@@ -9,6 +9,18 @@
   home-manager.users.${username} = {
     home.stateVersion = "25.11";
 
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = "firefox.desktop";
+        "text/xml" = "firefox.desktop";
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+        "x-scheme-handler/about" = "firefox.desktop";
+        "x-scheme-handler/unknown" = "firefox.desktop";
+      };
+    };
+
     gtk = {
       enable = true;
       cursorTheme = {
