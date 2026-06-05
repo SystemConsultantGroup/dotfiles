62b1664598862caa29c206da6b58fc7768c49dae
apersomany <aperso@aperso.dev>
2026-06-05 16:51:53 +0900

    feat: add feature toggles (mkEnableOption + mkIf) to modules

--- modules/base/default.nix
+++ modules/base/default.nix
@@ -1,81 +1,89 @@
 {
-  pkgs,
+  config,
   lib,
+  pkgs,
   ...
 }:
+let
+  cfg = config.dotfiles.base;
+in
 {
-  imports = [
-    ./user.nix
-  ];
+  options.dotfiles.base = {
+    podman.enable = lib.mkEnableOption "Podman container engine";
+    nixld.enable = lib.mkEnableOption "nix-ld for running foreign binaries";
+  };
 
-  nix = {
-    settings = {
-      experimental-features = [
-        "nix-command"
-        "flakes"
+  config = lib.mkMerge [
+    {
+      imports = [ ./user.nix ];
+
+      nix = {
+        settings = {
+          experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
+          auto-optimise-store = true;
+        };
+        gc = {
+          automatic = true;
+          dates = "weekly";
+          options = "--delete-older-than 7d";
+        };
+      };
+
+      nixpkgs.config.allowUnfree = true;
+      security.sudo.wheelNeedsPassword = false;
+      i18n.defaultLocale = "en_US.UTF-8";
+
+      networking = {
+        nameservers = [
+          "1.1.1.1"
+          "8.8.8.8"
+          "1.0.0.1"
+          "8.8.4.4"
+        ];
+        networkmanager.enable = lib.mkDefault true;
+        nftables.enable = true;
+        firewall.enable = false;
+      };
+
+      environment.systemPackages = [
+        pkgs.bat
+        pkgs.bun
+        pkgs.fd
+        pkgs.fzf
+        pkgs.git
+        pkgs.gh
+        pkgs.devenv
+        pkgs.jq
+        pkgs.mtr
+        pkgs.nh
+        pkgs.nodejs
+        pkgs.opencode
+        pkgs.ripgrep
+        pkgs.unzip
+        pkgs.uv
+        pkgs.yazi
+        pkgs.zip
       ];
-      auto-optimise-store = true;
-    };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
-  };
-
-  nixpkgs.config.allowUnfree = true;
-
-  security.sudo.wheelNeedsPassword = false;
-
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-  };
-
-  networking = {
-    nameservers = [
-      "1.1.1.1"
-      "8.8.8.8"
-      "1.0.0.1"
-      "8.8.4.4"
-    ];
-    networkmanager.enable = lib.mkDefault true;
-    nftables.enable = true;
-    firewall.enable = false;
-  };
-
-  programs.nix-ld = {
-    enable = true;
-    libraries = [
-      pkgs.zlib
-      pkgs.stdenv.cc.cc
-    ];
-  };
-
-  virtualisation.podman = {
-    enable = true;
-    dockerCompat = true;
-    dockerSocket.enable = true;
-    defaultNetwork.settings.dns_enabled = true;
-  };
-
-  environment.systemPackages = [
-    pkgs.bat
-    pkgs.bun
-    pkgs.fd
-    pkgs.fzf
-    pkgs.git
-    pkgs.gh
-    pkgs.devenv
-    pkgs.jq
-    pkgs.mtr
-    pkgs.nh
-    pkgs.nodejs
-    pkgs.opencode
-    pkgs.ripgrep
-    pkgs.unzip
-    pkgs.uv
-    pkgs.yazi
-    pkgs.zip
+    }
+    (lib.mkIf cfg.podman.enable {
+      virtualisation.podman = {
+        enable = true;
+        dockerCompat = true;
+        dockerSocket.enable = true;
+        defaultNetwork.settings.dns_enabled = true;
+      };
+    })
+    (lib.mkIf cfg.nixld.enable {
+      programs.nix-ld = {
+        enable = true;
+        libraries = [
+          pkgs.zlib
+          pkgs.stdenv.cc.cc
+        ];
+      };
+    })
   ];
-
 }
--- modules/client/default.nix
+++ modules/client/default.nix
@@ -1,54 +1,72 @@
 {
+  config,
+  lib,
   pkgs,
   inputs,
-  lib,
   ...
 }:
+let
+  cfg = config.dotfiles.client;
+in
 {
-  imports = [
-    inputs.home-manager.nixosModules.home-manager
-    ./home
-    ./font.nix
+  options.dotfiles.client = {
+    pipewire.enable = lib.mkEnableOption "PipeWire audio service";
+    gnomeKeyring.enable = lib.mkEnableOption "GNOME Keyring";
+    hyprland.enable = lib.mkEnableOption "Hyprland compositor";
+  };
+
+  config = lib.mkMerge [
+    {
+      imports = [
+        inputs.home-manager.nixosModules.home-manager
+        ./home
+        ./font.nix
+      ];
+      hardware.graphics.enable = true;
+      i18n.inputMethod = {
+        enable = true;
+        type = "kime";
+        package = lib.mkForce inputs.kime.packages.${pkgs.system}.default;
+      };
+      environment.systemPackages = [
+        pkgs.vesktop
+        pkgs.firefox
+        pkgs.brave
+        pkgs.zed-editor
+        pkgs.vscode-fhs
+        pkgs.nixd
+        pkgs.nil
+        pkgs.pwvucontrol
+        pkgs.bibata-cursors
+        pkgs.brightnessctl
+      ];
+      services.xserver.xkb = {
+        layout = "us";
+        options = "terminate:ctrl_alt_bksp";
+      };
+    }
+    (lib.mkIf cfg.gnomeKeyring.enable {
+      services.gnome.gnome-keyring.enable = true;
+    })
+    (lib.mkIf cfg.pipewire.enable {
+      services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        jack.enable = true;
+      };
+    })
+    (lib.mkIf cfg.hyprland.enable {
+      programs.hyprland = {
+        enable = true;
+        withUWSM = true;
+      };
+      services.greetd = {
+        enable = true;
+        useTextGreeter = true;
+        settings.default_session.command = "${pkgs.tuigreet}/bin/tuigreet --cmd 'uwsm start hyprland-uwsm.desktop'";
+      };
+    })
   ];
-  hardware.graphics.enable = true;
-  i18n.inputMethod = {
-    enable = true;
-    type = "kime";
-    package = lib.mkForce inputs.kime.packages.${pkgs.system}.default;
-  };
-  environment.systemPackages = [
-    pkgs.vesktop
-    pkgs.firefox
-    pkgs.brave
-    pkgs.zed-editor
-    pkgs.vscode-fhs
-    pkgs.nixd
-    pkgs.nil
-    pkgs.pwvucontrol
-    pkgs.bibata-cursors
-    pkgs.brightnessctl
-  ];
-  services = {
-    gnome.gnome-keyring.enable = true;
-    xserver.xkb = {
-      layout = "us";
-      options = "terminate:ctrl_alt_bksp";
-    };
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      jack.enable = true;
-    };
-    greetd = {
-      enable = true;
-      useTextGreeter = true;
-      settings.default_session.command = "${pkgs.tuigreet}/bin/tuigreet --cmd 'uwsm start hyprland-uwsm.desktop'";
-    };
-  };
-  programs.hyprland = {
-    enable = true;
-    withUWSM = true;
-  };
 }
