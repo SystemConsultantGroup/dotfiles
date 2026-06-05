93c5e4a2f367ed9f1894b06d3100d1c045b5f24d
apersomany <aperso@aperso.dev>
2026-06-05 18:49:52 +0900

    refactor: flatten dead option gating into direct config
    Only podman is genuinely optional. nix-ld, pipewire, gnome-keyring,
    and hyprland are always enabled with no host ever overriding them.
    Remove the mkOption/mkIf ceremony and inline directly.
    
    - modules/base/default.nix: inline nix-ld, keep only podman option
    - modules/client/default.nix: inline everything, remove all options
      (pipewire, gnomeKeyring, hyprland). Merge repeated services.* keys
      into single services = { ... } block per statix.

--- modules/client/default.nix
+++ modules/client/default.nix
@@ -1,77 +1,54 @@
 {
-  config,
   lib,
   pkgs,
   inputs,
   ...
 }:
-let
-  cfg = config.dotfiles.client;
-in
 {
   imports = [
     ./home
     ./font.nix
   ];
 
-  options.dotfiles.client = {
-    pipewire.enable = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Whether to enable PipeWire audio service";
+  hardware.graphics.enable = true;
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "kime";
+    package = lib.mkForce inputs.kime.packages.${pkgs.system}.default;
+  };
+
+  environment.systemPackages = [
+    pkgs.vscode-fhs
+    pkgs.bibata-cursors
+    pkgs.brightnessctl
+  ];
+
+  services = {
+    xserver.xkb = {
+      layout = "us";
+      options = "terminate:ctrl_alt_bksp";
     };
-    gnomeKeyring.enable = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Whether to enable GNOME Keyring";
+
+    gnome.gnome-keyring.enable = true;
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
     };
-    hyprland.enable = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Whether to enable Hyprland compositor";
+
+    greetd = {
+      enable = true;
+      useTextGreeter = true;
+      settings.default_session.command = "${pkgs.tuigreet}/bin/tuigreet --cmd 'uwsm start hyprland-uwsm.desktop'";
     };
   };
 
-  config = lib.mkMerge [
-    {
-      hardware.graphics.enable = true;
-      i18n.inputMethod = {
-        enable = true;
-        type = "kime";
-        package = lib.mkForce inputs.kime.packages.${pkgs.system}.default;
-      };
-      environment.systemPackages = [
-        pkgs.vscode-fhs
-        pkgs.bibata-cursors
-        pkgs.brightnessctl
-      ];
-      services.xserver.xkb = {
-        layout = "us";
-        options = "terminate:ctrl_alt_bksp";
-      };
-    }
-    (lib.mkIf cfg.gnomeKeyring.enable {
-      services.gnome.gnome-keyring.enable = true;
-    })
-    (lib.mkIf cfg.pipewire.enable {
-      services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-        jack.enable = true;
-      };
-    })
-    (lib.mkIf cfg.hyprland.enable {
-      programs.hyprland = {
-        enable = true;
-        withUWSM = true;
-      };
-      services.greetd = {
-        enable = true;
-        useTextGreeter = true;
-        settings.default_session.command = "${pkgs.tuigreet}/bin/tuigreet --cmd 'uwsm start hyprland-uwsm.desktop'";
-      };
-    })
-  ];
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+  };
 }
