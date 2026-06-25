abb923210e183b908e17d1db5de82300b82219f0
apersomany <aperso@aperso.dev>
2026-06-26 03:54:21 +0900

    chore: drop opencode alias (replaced by pi)

--- modules/base/home/bash.nix
+++ modules/base/home/bash.nix
@@ -10,7 +10,6 @@
         fi
 
         export PS1="\[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] "
-        alias opencode="bunx opencode-ai"
         alias pi="bunx @earendil-works/pi-coding-agent"
         export NIXPKGS_ALLOW_UNFREE=1
         export NIXPKGS_ACCEPT_ANDROID_SDK_LICENSE=1
